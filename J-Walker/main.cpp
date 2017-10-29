7aaa92445319bac6e1282afbd563fc3684c5d998
Alexander Thomas <6ftDwarf@gmail.com>
2017-10-29 16:52:01 -0700

    Add card system

--- J-Walker/main.cpp
+++ J-Walker/main.cpp
@@ -3,6 +3,7 @@
 #include <time.h>
 #include <string>
 #include <fstream>
+#include <vector>
 
 #include "sfwdraw.h"
 #include "MathLib\Transform.h"
@@ -31,17 +32,22 @@ GameState state = WORLD_STATE;
 Player player;
 World world;
 MapRegion newReg;
+bool usingCard = false;
 int editTileType = 0;
 int editSpawner = 0;
 bool editTilesNotEntities = true;
 std::string newRegionDesc = "Not set";
+std::vector<int> deck;
+int deckIdx = -1;
+int newCardType = 1;
 
 Button tileTypeBut;
+Button cardTypeBut;
 
 
 void switchToAvatarMode();
 void switchToWorldMode();
-void genRegion();
+void genRegion(int type);
 void selectGrass();
 void selectWater();
 void selectEmpty();
@@ -49,12 +55,19 @@ void selectSpawnOne();
 void selectSpawnTwo();
 void selectSpawnThree();
 
+void drawCard();
+void nextCardType();
+void prevCardType();
+void setCardType();
+
 void previousTileType();
 void nextTileType();
 void selectTiles();
 
 void saveWorld();
 void loadWorld();
+
+void shuffleDeck();
 
 
 
@@ -74,6 +87,12 @@ int main()
 	MouseHandler::init();
 
 	newReg.tileCounts[0] = -1;
+	deck.push_back(1);
+	deck.push_back(1);
+	deck.push_back(2);
+	deck.push_back(2);
+	shuffleDeck();
+	deck.push_back(0);
 
 
 
@@ -97,7 +116,7 @@ int main()
 	genRegBut.transform.pos = { 100, 0 };
 	genRegBut.size = { 100, 50 };
 	genRegBut.transform.disfigure = genRegBut.size;
-	genRegBut.callback = &genRegion;
+	genRegBut.callback = &drawCard;
 	genRegBut.imgs[0] = TextureLoader::gen_button_1;
 	genRegBut.imgs[1] = TextureLoader::gen_button_2;
 
@@ -189,6 +208,39 @@ int main()
 	loadButton.colors[0] = WHITE;
 	loadButton.colors[1] = GREEN;
 
+	//Button cardTypeBut;
+	cardTypeBut.transform.pos = { 450, 25 };
+	cardTypeBut.size = { 50, 50 };
+	cardTypeBut.transform.disfigure = cardTypeBut.size;
+	cardTypeBut.callback = &setCardType;
+	cardTypeBut.useColors = true;
+	cardTypeBut.imgs[0] = TextureLoader::empty_tile_img;
+	cardTypeBut.imgs[1] = TextureLoader::empty_tile_img;
+	cardTypeBut.colors[0] = WHITE;
+	cardTypeBut.colors[1] = GREEN;
+
+	Button prevCardTypeBut;
+	prevCardTypeBut.transform.pos = { 400, 25 };
+	prevCardTypeBut.size = { 50, 50 };
+	prevCardTypeBut.transform.disfigure = prevCardTypeBut.size;
+	prevCardTypeBut.callback = &prevCardType;
+	prevCardTypeBut.useColors = true;
+	prevCardTypeBut.imgs[0] = TextureLoader::leftArrowImg;
+	prevCardTypeBut.imgs[1] = TextureLoader::leftArrowImg;
+	prevCardTypeBut.colors[0] = WHITE;
+	prevCardTypeBut.colors[1] = GREEN;
+
+	Button nextCardTypeBut;
+	nextCardTypeBut.transform.pos = { 500, 25 };
+	nextCardTypeBut.size = { 50, 50 };
+	nextCardTypeBut.transform.disfigure = nextCardTypeBut.size;
+	nextCardTypeBut.callback = &nextCardType;
+	nextCardTypeBut.useColors = true;
+	nextCardTypeBut.imgs[0] = TextureLoader::rightArrowImg;
+	nextCardTypeBut.imgs[1] = TextureLoader::rightArrowImg;
+	nextCardTypeBut.colors[0] = WHITE;
+	nextCardTypeBut.colors[1] = GREEN;
+
 	
 
 
@@ -207,70 +259,113 @@ int main()
 
 
 
-			if (newReg.tileCounts[0] != -1)
+			if (usingCard)
 			{
-
-				vec2 selectPos = { sfw::getMouseX() - 400, sfw::getMouseY() - 300 };
-				selectPos = selectPos + player.transform.pos;
-				selectPos = {floor((selectPos.x) / 40) * 40,
-							 floor((selectPos.y) / 40) * 40 };
-
-				if (newReg.spawners[0].usable)
+				if (deck[deckIdx] != 0)
 				{
-					spawnerButOne.draw();
-				}
-				if (newReg.spawners[1].usable)
-				{
-					spawnerButTwo.draw();
-				}
-				if (newReg.spawners[2].usable)
-				{
-					spawnerButThree.draw();
-				}
+					vec2 selectPos = { sfw::getMouseX() - 400, sfw::getMouseY() - 300 };
+					selectPos = selectPos + player.transform.pos;
+					selectPos = { floor((selectPos.x) / 40) * 40,
+								 floor((selectPos.y) / 40) * 40 };
 
-
-				vec2 gridPos = selectPos / 40;
-				newReg.transform.pos = gridPos;
-
-				bool found = false;
-				bool valid = true;
-
-				float goodDist = distance({ 1, 0 }, { 0, 0 });
-
-				for (int i = 0; i < world.regs.size(); ++i)
-				{
-					vec2 regPos = world.regs[i].transform.pos;
-					float dist = distance(regPos, gridPos);
-					if (regPos.x == gridPos.x && regPos.y == gridPos.y)
+					if (newReg.spawners[0].usable)
 					{
-						valid = false;
+						spawnerButOne.draw();
+					}
+					if (newReg.spawners[1].usable)
+					{
+						spawnerButTwo.draw();
+					}
+					if (newReg.spawners[2].usable)
+					{
+						spawnerButThree.draw();
+					}
+
+
+					vec2 gridPos = selectPos / 40;
+					newReg.transform.pos = gridPos;
+
+					bool found = false;
+					bool valid = true;
+
+					float goodDist = distance({ 1, 0 }, { 0, 0 });
+
+					for (int i = 0; i < world.regs.size(); ++i)
+					{
+						vec2 regPos = world.regs[i].transform.pos;
+						float dist = distance(regPos, gridPos);
+						if (regPos.x == gridPos.x && regPos.y == gridPos.y)
+						{
+							valid = false;
+							break;
+						}
+						else if (dist == goodDist)
+						{
+							found = true;
+						}
+					}
+
+
+					if (MouseHandler::leftDownStart)
+					{
+
+						if (valid && found)
+						{
+							state = EDIT_STATE;
+							//editTileType = 0;
+							usingCard = false;
+							editTilesNotEntities = true;
+							//tileTypeBut.imgs[0] = TextureLoader::empty_tile_img;
+							//tileTypeBut.imgs[1] = TextureLoader::empty_tile_img;
+						}
+					}
+
+					int drawColor = (valid && found) ? GREEN : WHITE;
+
+					Box::draw(player.transform.pos, selectPos, { 40, 40 }, drawColor);
+					sfw::drawString(TextureLoader::stringBitmap, newRegionDesc.c_str(), 200, 50, 20, 20);
+				}
+				else
+				{
+					cardTypeBut.update();
+					nextCardTypeBut.update();
+					prevCardTypeBut.update();
+
+					std::string cardDesc;
+					switch (newCardType)
+					{
+					case 0:
+						cardDesc = "New card";
+						break;
+					case 1:
+						cardDesc = "Grassland region";
+						break;
+					case 2:
+						cardDesc = "Ocean region";
+						break;
+					case 3:
+						cardDesc = "Forest region";
+						break;
+					case 4:
+						cardDesc = "Mountain region";
+						break;
+					case 5:
+						cardDesc = "Desert region";
+						break;
+					case 6:
+						cardDesc = "Marsh region";
+						break;
+					default:
+						cardDesc = "Dev forgot to add a desc";
 						break;
 					}
-					else if (dist == goodDist)
-					{
-						found = true;
-					}
+
+					sfw::drawString(TextureLoader::stringBitmap, "Add a new card", 10, 100, 15, 15);
+					sfw::drawString(TextureLoader::stringBitmap, cardDesc.c_str(), 250, 100, 20, 20);
+					cardTypeBut.draw();
+					nextCardTypeBut.draw();
+					prevCardTypeBut.draw();
 				}
-
-
-				if (sfw::getMouseButton(0))
-				{
-
-					if (valid && found)
-					{
-						state = EDIT_STATE;
-						//editTileType = 0;
-						editTilesNotEntities = true;
-						//tileTypeBut.imgs[0] = TextureLoader::empty_tile_img;
-						//tileTypeBut.imgs[1] = TextureLoader::empty_tile_img;
-					}
-				}
-
-				int drawColor = (valid && found) ? GREEN : WHITE;
-
-				Box::draw(player.transform.pos, selectPos, { 40, 40 }, drawColor);
-				sfw::drawString(TextureLoader::stringBitmap, newRegionDesc.c_str(), 200, 50, 20, 20);
-
 			}
 
 			avatarModeBut.draw();
@@ -393,7 +488,13 @@ int main()
 			}
 		}
 
-
+		/*std::string decks;
+		for (int i = 0; i < deck.size(); ++i)
+		{
+			decks.append(std::to_string(deck[i]));
+			decks.append(",");
+		}
+		std::cout << decks << std::endl;*/
 	}
 
 	return 0;
@@ -427,7 +528,7 @@ void switchToWorldMode()
 	world.regs.push_back(newReg);
 }
 
-void genRegion()
+void genRegion(int type)
 {
 	if (newReg.tileCounts[0] == -1)
 	{
@@ -437,7 +538,7 @@ void genRegion()
 			newReg.spawners[i].usable = false;
 		}
 
-		int regionType = rand() % 6;
+		//int regionType = rand() % 6;
 		int regFillType = 0;
 		/*
 		region types
@@ -449,9 +550,10 @@ void genRegion()
 		5 marsh
 		*/
 
+		usingCard = true;
 		newReg.tileCounts[0] = 100;
 
-		switch (regionType)
+		switch (type)
 		{
 		case 0:
 			newReg.tileCounts[1] = 0;
@@ -702,4 +804,93 @@ void loadWorld()
 	}
 
 	file.close();
+}
+
+
+
+
+
+void drawCard()
+{
+	if (!usingCard)
+	{
+		++deckIdx;
+		if (deckIdx == deck.size())
+		{
+			shuffleDeck();
+			deckIdx = 0;
+		}
+		usingCard = true;
+		if (deck[deckIdx] != 0)
+		{
+			genRegion(deck[deckIdx] - 1);
+		}
+		else
+		{
+			newCardType = 0;
+			cardTypeBut.imgs[0] = TileTypes::tileTypes[newCardType].texId;
+			cardTypeBut.imgs[1] = TileTypes::tileTypes[newCardType].texId;
+		}
+	}
+}
+
+void nextCardType()
+{
+	++newCardType;
+	if (newCardType > 6)
+	{
+		newCardType = 0;
+	}
+	if (newCardType != 6)
+	{
+		cardTypeBut.imgs[0] = TileTypes::tileTypes[newCardType].texId;
+		cardTypeBut.imgs[1] = TileTypes::tileTypes[newCardType].texId;
+	}
+	else
+	{
+		cardTypeBut.imgs[0] = TileTypes::tileTypes[7].texId;
+		cardTypeBut.imgs[1] = TileTypes::tileTypes[7].texId;
+	}
+}
+
+void prevCardType()
+{
+	--newCardType;
+	if (newCardType < 0)
+	{
+		newCardType = 6;
+	}
+	if (newCardType != 6)
+	{
+		cardTypeBut.imgs[0] = TileTypes::tileTypes[newCardType].texId;
+		cardTypeBut.imgs[1] = TileTypes::tileTypes[newCardType].texId;
+	}
+	else
+	{
+		cardTypeBut.imgs[0] = TileTypes::tileTypes[7].texId;
+		cardTypeBut.imgs[1] = TileTypes::tileTypes[7].texId;
+	}
+}
+
+void setCardType()
+{
+	deck.push_back(newCardType);
+	shuffleDeck();
+	usingCard = false;
+}
+
+void shuffleDeck()
+{
+	int temp = 0;
+	int toSwitch = 0;
+
+	for (int i = 0; i < deck.size(); ++i)
+	{
+		toSwitch = rand() % deck.size();
+		temp = deck[i];
+		deck[i] = deck[toSwitch];
+		deck[toSwitch] = temp;
+	}
+
+	deckIdx = -1;
 }
