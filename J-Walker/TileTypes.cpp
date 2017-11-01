cd4c13a80c0a9b260a5352ed5cd12b5f4377b8f0
axthomas-aie <s179069@students.aie.edu.au>
2017-11-01 12:21:36 -0700

    Tweak food & metal values

--- J-Walker/TileTypes.cpp
+++ J-Walker/TileTypes.cpp
@@ -23,49 +23,49 @@ TileTypes::TileTypes()
 	MapTile grass;
 	grass.name = "grass";
 	grass.texId = TextureLoader::grassImg;
-	grass.food = 10;
-	grass.metal = 1;
+	grass.food = 0.25f;
+	grass.metal = 0;
 	tileTypes.push_back(grass);
 
 	MapTile water;
 	water.name = "water";
 	water.texId = TextureLoader::waterImg;
-	water.food = 20;
+	water.food = 1;
 	water.metal = 0;
 	tileTypes.push_back(water);
 
 	MapTile forest;
 	forest.name = "forest";
 	forest.texId = TextureLoader::forestImg;
-	forest.food = 12;
+	forest.food = 0.5f;
 	forest.metal = 0;
 	tileTypes.push_back(forest);
 
 	MapTile mountain;
 	mountain.name = "mountain";
 	mountain.texId = TextureLoader::mountainImg;
-	mountain.food = 0.1f;
-	mountain.metal = 10;
+	mountain.food = 0.01f;
+	mountain.metal = 1;
 	tileTypes.push_back(mountain);
 
 	MapTile desert;
 	desert.name = "desert";
 	desert.texId = TextureLoader::desertImg;
-	desert.food = 0.25f;
-	desert.metal = 2;
+	desert.food = 0.025f;
+	desert.metal = 0.05f;
 	tileTypes.push_back(desert);
 
 	MapTile hill;
 	hill.name = "hill";
 	hill.texId = TextureLoader::hillImg;
-	hill.food = 0.75f;
-	hill.metal = 1.5f;
+	hill.food = 0.075f;
+	hill.metal = 0.25f;
 	tileTypes.push_back(hill);
 
 	MapTile marsh;
 	marsh.name = "marsh";
 	marsh.texId = TextureLoader::marshImg;
-	marsh.food = 2;
+	marsh.food = 0.2f;
 	marsh.metal = 0;
 	tileTypes.push_back(marsh);
 }
